1f16f2f5f01f0be50473fbfd7cb86d314b8719a2
Jared Mygrant <jamrow@gmail.com>
2018-03-17 11:28:39 -0600

    001 - Adding initial files into my repo for the TreeLandJourney Project. Implemented my Node class. Marked the obj, bin, and .vs files as git ignore

---
+++ TreeLandJorneyProject/TreeLandJorneyProject/Node.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreeLandJorneyProject
+{
+    public class Node
+    {
+		public int Name { get; set; }
+		private List<int> roadList;
+
+		public Node()
+		{
+			roadList = new List<int>();
+		}
+
+		public List<int> RoadList
+		{
+			get { return roadList; }
+		}
+
+		public int RoadListCount()
+		{
+			return roadList.Count;
+		}
+
+		public void AddNewRoadToRoadList(int newRoad)
+		{
+			if(newRoad == Name)
+			{
+				return;
+			}
+
+			if(newRoad < 0)
+			{
+				return;
+			}
+
+			if(!roadList.Contains(newRoad))
+			{
+				roadList.Add(newRoad);
+			}
+		}
+    }
+}
